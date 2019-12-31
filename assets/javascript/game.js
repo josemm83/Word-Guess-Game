b87afbb418bfda55926921f907d4026ae74418a6
josemm83 <josemmancha@gmail.com>
2019-12-31 00:43:33 -0600

    updating files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,64 @@
-var wordIndex = ["the skywalkers", "luke skywalker", "han solo", "leia organa", "yoda", "obi wan kenobi", "darth vader", "darth sidious", 
+var wordIndex = ["the skywalkers", "luke skywalker", "han solo", "leia organa", "master yoda", "obi wan kenobi", "darth vader", "darth sidious", 
      "lando calrissian", "chewbacca"]; //10
 var countdown = 15;
 var numRight = 0;
 var phrase = [];
 var guessedLtrs = [];
-var ltr = /^[a-zA-z]+$/;
+var wins = 0;
+// var ltr = /^[a-zA-z]+$/;
 //return a random index number for wordIndex
 var guess = wordIndex[Math.floor(Math.random() * wordIndex.length)]; //selects guessed word of the index
 // console.log(index);
  console.log(guess);
 // console.log(guess.length);
-
+var exactLength = guess.length;
 //will add 
 for (var j = 0; j < guess.length; j++){
-    phrase[j] = " _ ";
+    if(guess.charAt(j) === " "){
+        exactLength --;
+        phrase[j] = " ";
+    }
+    else{
+        phrase[j] = " _ ";
+    }
 }
 
 function gameStart(ltr){
-    if(ltr === guess.charAt(i)){
-        phrase[i] = ltr;
-        numRight ++;
+    for(var i = 0; i < guess.length; i++){
+        if(ltr === guess.charAt(i)){
+            phrase[i] = ltr;
+            numRight ++;
+            document.querySelector("#result").innerHTML = phrase.join(" ");
         }
-    document.querySelector("#result").innerHTML = phrase;
+        if (numRight === exactLength){
+            printout();
+            return;
+        }
+    }
+    guessedLtrs.push(ltr);
+}
+
+function printout(){
+    document.querySelector("#wins").innerHTML = "Number of wins: " + wins;
+    document.querySelector("#guess").innerHTML = "Guessed Letters: " + guessedLtrs.join(" ");
+    document.querySelector("#remaining").innerHTML = "Number of guesses remaining: " + countdown;
 }
 //activates when user presses a key
 document.onkeyup = function(event){
-    if(numRight === guess.length){
+    // console.log(numRight);
+    // console.log(exactLength);
+    if(numRight === exactLength){
         document.querySelector("#update").innerHTML = "Congratulations You Have Solved The Puzzle";
+        wins ++;
+        printout();
         return;
     }
-    else if(countdown = 0){
+    else if(countdown === 0){
         document.querySelector("#update").innerHTML = "Better Luck Next Time Kiddo...";
         return;
     }
     var letter = event.key.toLowerCase();
     gameStart(letter);
     countdown --;
+    printout();
 };
